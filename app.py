ca25a040365ba891365c979719e86d7ad40b6d81
Paula-JS <paulaajs20@gmail.com>
2024-12-19 11:23:09 +0100

    second commit

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ from langchain_core.output_parsers import StrOutputParser #la respuesta que devu
 from langchain_core.runnables import RunnablePassthrough 
 from langchain_google_genai import ChatGoogleGenerativeAI #clase del LLM que estamos usando
 from langchain.schema import Document
+import PyPDF2
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 from dotenv import load_dotenv
 load_dotenv()
-
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
 
 st.set_page_config(
     page_title="LangChain APP",
@@ -27,39 +27,28 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-st.title("LangChain APP 📖🔎")
-st.write("¡Bienvenidos a nuestra aplicación interactiva desarrollada con Streamlit! Esta herramienta te permite cargar un archivo PDF y, mediante procesamiento de lenguaje natural, puedes hacerle preguntas sobre el contenido del documento. Ya sea para extraer información clave o resolver dudas específicas, nuestra app facilita la interacción y comprensión de los archivos PDF de manera rápida y sencilla. ¡Explora el contenido de tus documentos de una forma más eficiente y accesible! ✌🏼")
+#file = st.file_uploader("Upload a PDF file", accept_multiple_files=False, type="pdf")
 
-file = st.file_uploader("Inserta un documento PDF", type="pdf")
+source_data_folder = "./content/MisDatos"
+# Leyendo los PDFs del directorio configurado
+loader = PyPDFDirectoryLoader(source_data_folder)
+data_on_pdf = loader.load()
 
-import PyPDF2
+# Particionando los datos. Con un tamaño delimitado (chunks) y 
+# 200 caracters de overlapping para preservar el contexto
+text_splitter = RecursiveCharacterTextSplitter(
+    separators=["\n\n", "\n", ". ", " ", ""], #para que corte las frases por ahi
+    chunk_size=1000,
+    chunk_overlap=200
+)
+splits = text_splitter.split_documents(data_on_pdf)
 
-import PyPDF2
-
-splits = []
-if file is not None:
-    # Read the PDF file
-    pdf_reader = PyPDF2.PdfReader(file)
-    # Extract the content
-    content = ""
-    for page in pdf_reader.pages:
-        content += page.extract_text()
-    # Display the content
-
-    documents = [Document(page_content=content)]
-    # 200 caracters de overlapping para preservar el contexto
-    text_splitter = RecursiveCharacterTextSplitter(
-        separators=["\n\n", "\n", ". ", " ", ""], #para que corte las frases por ahi
-        chunk_size=1000,
-        chunk_overlap=200
-    )
-    splits = text_splitter.split_documents(documents)
-        
 from langchain.vectorstores import Chroma
 from langchain.embeddings import CohereEmbeddings
 
 # Crea la instancia de embeddings con Cohere
-embeddings_model = CohereEmbeddings(cohere_api_key=os.environ["API_KEY_COHERE"], user_agent="antonio")
+
+embeddings_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=os.environ["API_KEY_GOOGLE"])
 
 path_db = "./content/VectorDB"  # Ruta a la base de datos del vector store
 
@@ -67,14 +56,14 @@ path_db = "./content/VectorDB"  # Ruta a la base de datos del vector store
 vectorstore = Chroma.from_documents(
     documents=splits, 
     embedding=embeddings_model, 
-    persist_directory=path_db
+    persist_directory=path_db #esto lo que haces es guardarlo en el disco duro
 )
 
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", google_api_key=os.environ["API_KEY_GOOGLE"])
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", google_api_key=os.environ["API_KEY_GOOGLE"]) #para que esto funcione tiene que estra en los secretos
 
 retriever = vectorstore.as_retriever()
 
-prompt = hub.pull("rlm/rag-prompt")
+prompt = hub.pull("rlm/rag-prompt") #parte del langchain que recoge informacion para pasarsela al modelo
 
 def format_docs(docs):
     # Funcion auxiliar para enviar el contexto al modelo como parte del prompt
@@ -87,18 +76,16 @@ rag_chain = (
     | StrOutputParser()
 )
 
-prompt.messages[0].prompt.template = "You are a Bolivian kitcken expert assistant for question-answering tasks (thus you might wanna answer in Spanish). Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.\nQuestion: {question} \nContext: {context} \nAnswer:"
+pregunta = "Que es la pacobilla? " # @param {type:"string"}
+response = rag_chain.invoke(pregunta )
+print(response)
 
-user_input = st.text_input("Hazme una pregunta")
+st.title("LangChain APP 📖🔎")
+st.write("¡Bienvenidos a nuestra aplicación interactiva desarrollada con Streamlit! Esta herramienta te permite cargar un archivo PDF y, mediante procesamiento de lenguaje natural, puedes hacerle preguntas sobre el contenido del documento. Ya sea para extraer información clave o resolver dudas específicas, nuestra app facilita la interacción y comprensión de los archivos PDF de manera rápida y sencilla. ¡Explora el contenido de tus documentos de una forma más eficiente y accesible! ✌🏼")
 
+pregunta = st.text_input("ASK:", "Ques es la pacobilla?")
 
-if st.button("RUN QUERY", type="primary"):
-    if user_input:
-        response = rag_chain.invoke(user_input)
-        Markdown(response)
-
-        st.write("Query: " + user_input)
-        st.header("Generated Answer: " )
-        st.write("Retrieved chunk: " + response)
-
-
+if st.button ("RUN QUERY"):
+    responde = rag_chain.invoke(pregunta)
+    print(response)
+    st.write(response) 
